ae4e00dc634e222a86aa3e061c23122eb495d6ac
Nitin Karmuse <nkarmuse@gmail.com>
2013-06-03 22:06:51 +0100

    test heroku build

--- grails-app/controllers/emote/GitAutoController.groovy
+++ grails-app/controllers/emote/GitAutoController.groovy
@@ -3,6 +3,7 @@ package emote
 class GitAutoController {
 
     def index() {
+		log.info "executing git auto"
 		def process = "git pull git@github.com:dhsanket/emote.git".execute()
 		render ("output - ${process.text}")
 	}
