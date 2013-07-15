bbcc906b717288975e9461302cb6a080ef73e83a
dhsanket <sanketd367@gmail.com>
2013-07-15 17:08:04 +0100

    Set-up datasource for test and production

--- grails-app/conf/DataSource.groovy
+++ grails-app/conf/DataSource.groovy
@@ -1,11 +1,6 @@
 grails {
 	mongo {
-		databaseName = "app14658629"
-		dbCreate = "update"
-		host = "dharma.mongohq.com"
-		port = 10046
-		username = "emote-app"
-		password = "Emote-101"
+
 	}
   }
 
@@ -13,16 +8,22 @@ grails {
 environments {
 	development {
 		mongo {
+			databaseName = "app15169332"
 			dbCreate = "update"
-			databaseName = "emote"
-			host = "localhost"
-			port = 27017
+			host = "linus.mongohq.com"
+			port = 10001
+			username = "emote-app"
+			password = "Emote-101"
 		}
 	}
 	test {
 		mongo {
+			databaseName = "app15169332"
 			dbCreate = "update"
-
+			host = "linus.mongohq.com"
+			port = 10001
+			username = "emote-app"
+			password = "Emote-101"	
 		}
 	}
 	production {
@@ -32,8 +33,7 @@ environments {
 			host = "dharma.mongohq.com"
 			port = 10046
 			username = "emote-app"
-			password = "Emote-101"
-	
+			password = "Emote-101"	
 		}
 	}
 }
