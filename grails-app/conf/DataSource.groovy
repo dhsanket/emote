1a511550f84f56063966b02c28109d23f116eacf
Nitin Karmuse <nkarmuse@gmail.com>
2013-07-21 15:44:26 +0100

    make mongo db for diff env to work

--- grails-app/conf/DataSource.groovy
+++ grails-app/conf/DataSource.groovy
@@ -1,36 +1,37 @@
- grails {
-   mongo {
-
-   }
-   }
 // environment specific settings
 environments {
 	development {
-		mongo {
-			dbCreate = "update"
-			databaseName = "emote"
-			host = "localhost"
-			port = 27017
+		grails{
+			mongo {
+				dbCreate = "update"
+				databaseName = "emote"
+				host = "localhost"
+				port = 27017
+			}
 		}
 	}
 	test {
-		mongo {
-			databaseName = "app15169332"
-			dbCreate = "update"
-			host = "linus.mongohq.com"
-			port = 10001
-			username = "emote-app"
-			password = "Emote-101"	
+		grails{
+			mongo {
+				databaseName = "app15169332"
+				dbCreate = "update"
+				host = "linus.mongohq.com"
+				port = 10001
+				username = "emote-app"
+				password = "Emote-101"	
+			}
 		}
 	}
 	production {
-		mongo {
-			databaseName = "app14658629"
-			dbCreate = "update"
-			host = "dharma.mongohq.com"
-			port = 10046
-			username = "emote-app"
-			password = "Emote-101"	
+		grails{
+			mongo {
+				databaseName = "app14658629"
+				dbCreate = "update"
+				host = "dharma.mongohq.com"
+				port = 10046
+				username = "emote-app"
+				password = "Emote-101"	
+			}
 		}
 	}
 }
