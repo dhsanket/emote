a4b62de7ada6115a7bfbfbb986e68c26959832c9
Mehdi Ostadhashem <m_ostadhshem@odesk.com>
2013-09-28 13:42:28 +0400

    Fix bug about logging

--- grails-app/conf/Config.groovy
+++ grails-app/conf/Config.groovy
@@ -104,7 +104,7 @@ log4j = {
     appenders {
         file name:'error', layout:pattern(conversionPattern: '%c{2} %m%n'), maxSize: 1024, file:"error.log",
                 threshold: org.apache.log4j.Level.ERROR
-
+        console name: 'stdout', layout: pattern(conversionPattern: '%c{2} %m%n')
     }
 
 	debug  'com.emote' // emote classes
@@ -121,7 +121,6 @@ log4j = {
            'net.sf.ehcache.hibernate'
 	   root{
 		    info 'stdout'
-            error 'error'
 	   }
 	   
 }
