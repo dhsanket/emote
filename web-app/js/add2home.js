daea9b0397dde59d2883fc4c607c55995e2b9fdc
dhsanket <sanketd367@gmail.com>
2013-11-23 20:30:17 +0000

    minor correction

--- web-app/js/add2home.js
+++ web-app/js/add2home.js
@@ -27,12 +27,12 @@ var addToHome = (function (w) {
 			returningVisitor: false,	// Show the balloon to returning visitors only (setting this to true is highly recommended)
 			animationIn: 'drop',		// drop || bubble || fade
 			animationOut: 'fade',		// drop || bubble || fade
-			startDelay: 2000,			// 2 seconds from page load before the balloon appears
+			startDelay: 8000,			// 2 seconds from page load before the balloon appears
 			lifespan: 15000,			// 15 seconds before it is automatically destroyed
 			bottomOffset: 14,			// Distance of the balloon from bottom
 			expire: 0,					// Minutes to wait before showing the popup again (0 = always displayed)
 			message: '',				// Customize your message or force a language ('' = automatic)
-			touchIcon: false,			// Display the touch icon
+			touchIcon: true,			// Display the touch icon
 			arrow: true,				// Display the balloon arrow
 			hookOnLoad: true,			// Should we hook to onload event? (really advanced usage)
 			closeButton: true,			// Let the user close the balloon
