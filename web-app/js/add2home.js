5d935efabff158f3d541dbc8032e2885e6db1394
dhsanket <sanketd367@gmail.com>
2013-11-23 20:56:01 +0000

    minor CSS change for 'add to homescreen' iOS popup

--- web-app/js/add2home.js
+++ web-app/js/add2home.js
@@ -28,7 +28,7 @@ var addToHome = (function (w) {
 			animationIn: 'drop',		// drop || bubble || fade
 			animationOut: 'fade',		// drop || bubble || fade
 			startDelay: 8000,			// 2 seconds from page load before the balloon appears
-			lifespan: 15000,			// 15 seconds before it is automatically destroyed
+			lifespan: 60000,			// 15 seconds before it is automatically destroyed
 			bottomOffset: 14,			// Distance of the balloon from bottom
 			expire: 0,					// Minutes to wait before showing the popup again (0 = always displayed)
 			message: '',				// Customize your message or force a language ('' = automatic)
