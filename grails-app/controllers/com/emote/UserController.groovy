b0f804b1b20179262bcfdba1b83e11a174bd7045
dhsanket <sanketd367@gmail.com>
2013-11-24 21:25:30 +0000

    desktop landing page tweak6

--- grails-app/controllers/com/emote/UserController.groovy
+++ grails-app/controllers/com/emote/UserController.groovy
@@ -72,7 +72,7 @@ class UserController
 			return
 		}
 		log.info " did not find facebook Context"
-		redirect(controller:'user' , action:'signin')
+		redirect(controller:'emote' , action:'index')
 		return ;
 	}
 	
