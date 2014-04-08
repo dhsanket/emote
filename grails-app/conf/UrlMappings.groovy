968d035f5d1fadd577017da0ce93f1eb3883f9a5
Hernan <hernan.yamakawa@gmail.com>
2014-04-08 16:52:41 -0300

    Fixed the default error page for 500 response code error

--- grails-app/conf/UrlMappings.groovy
+++ grails-app/conf/UrlMappings.groovy
@@ -1,13 +1,15 @@
 class UrlMappings {
 
 	static mappings = {
+        "500"(view: 'error')
+
 		"/$controller/$action?/$id?"{
 			constraints {
 				// apply constraints here
 			}
 		}
 		"/"(controller:"emote", action:"index")
-		"500"(view:'/error')
+
 		"/index.gsp"(view:"/index") // <-- added this line for Grails 2.0.0 / WL 10.3.4
 		"/zen/$titleString"(controller:"emote",action:"singleTitle")
 	}
